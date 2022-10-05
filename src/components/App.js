c6e8a64ce9cf0f26c5a951ce4cc9b3355d791e0b
Caio <caiosobreira77@gmail.com>
2022-10-04 21:53:03 -0300

    feat: Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Header from './layouts/Header';
 
 function App() {
 
   return (
     <BrowserRouter>
-
+      <Header />
       <Routes>
         <Route exact path="/" element={< teste/>} />
       </Routes>
